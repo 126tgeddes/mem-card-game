17d9f492ab1e1c708dfefeebbc14fba2294de3f4
Timothy <126tgeddes@frhsd.com>
2025-04-01 13:48:15 -0400

    4/1/25 fixed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 const cards = document.querySelectorAll(".card");
 
-let matchedCard = 0;
+let matchedCards = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
 function flipCard(e) {
-    let clickedCard = e.target; 
-    if(clickedCard !== cardOne && !disableDeck) {
+    let clickedCard = e.target;
+    if(clickedCard !== cardOne && !disableDeck){
         clickedCard.classList.add("flip");
-    if(!cardOne) {
-        return cardOne = clickedCard;
-    }
-    cardTwo = clickedCard;
-    disableDeck = true;
-    let cardOneImg = cardOne.querySelector("img").src,
-    cardTwoImg = cardTwo.querySelector("img").src;
-    matchCards(cardOneImg, cardTwoImg);
+        if(!cardOne){
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector("img").src,
+        cardTwoImg = cardTwo.querySelector("img").src;
+        matchCards(cardOneImg, cardTwoImg);
     }
     
 }
 
-function matchCards(img1, img2) {
+function matchCards(img1, img2){
     if(img1 === img2) {
-        matchedCard++;
-        if(matchedCard == 8) {
+        matchedCards++;
+        if(matchedCards == 8) {
             setTimeout(() => {
-                return shuffleCard();
+            return shuffleCard();
             }, 1000);
         }
         cardOne.removeEventListener("click", flipCard);
@@ -33,12 +33,13 @@ function matchCards(img1, img2) {
         cardOne = cardTwo = "";
         return disableDeck = false;
     }
-    
+
     setTimeout(() => {
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
     }, 400);
-
+    
+    
     setTimeout(() => {
         cardOne.classList.remove("shake", "flip");
         cardTwo.classList.remove("shake", "flip");
@@ -50,17 +51,19 @@ function matchCards(img1, img2) {
 function shuffleCard(){
     matchedCard = 0;
     cardOne = cardTwo = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,];
+    disableDeck = false;
+    let arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-    cards.forEach(card => {
+    cards.forEach((card,index) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector("img");
-        imgTag
+        imgTag.src = `img/img-${arr[index]}.png`;
         card.addEventListener("click", flipCard);
-    });
+    })
 }
 
+shuffleCard();
+
 cards.forEach(card => {
-    card.classList.add("flip");
     card.addEventListener("click", flipCard);
-});
+})
